0c2d9ae91c25b8172657e606b6d5c961b57e4fd2
Manoj Chambarge <42155660+chambargemanoj@users.noreply.github.com>
2020-04-10 07:33:45 -0500

    sample React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const name = "manoj";
-const currentdate = new Date();
-// const year = currentdate.getFullYear()
+
+const headcolor = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+headcolor.color = "green";
+const img1 =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMIarETVRyn7MITVFuxqkbDBp1ykjs4PrH6bFDg48tYIZq-XTf&usqp=CAU";
 ReactDOM.render(
   <div>
-    <p> Created By {name} </p>
-    <p> Copyright Current year {currentdate.getFullYear()} </p>
+    <h1 style={headcolor}> My favourite indian food </h1>
+    <div>
+      <img className="food-img" src={img1} alt="Idli" />
+      <img
+        className="food-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_FeNHjZ0C05WHVIvlOfNR_cE2zKcBdP55rA8VzkLHfT9XlSlC&usqp=CAU"
+        alt="sweet"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
